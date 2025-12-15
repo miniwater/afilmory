ef6fced56a11a0e61c5673b17a5782207b7c2adc
miniwater <miniwater@users.noreply.github.com>
2025-12-15 03:48:07 +0000

    deploy: 255b0d25a97e9b716efb683b156abe4a7461a188

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/afilmory/sw.js', { scope: '/afilmory/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/afilmory/' })})}
